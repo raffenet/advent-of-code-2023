4cbec1d85c700ddff54e1fa2ec75eab7d7411636
Ken Raffenetti <raffenet@mcs.anl.gov>
2023-12-02 10:19:53 -0600

    day2: Combine both parts

--- day2/day2.py
+++ day2/day2.py
@@ -9,29 +9,10 @@ ref = {
 }
 
 sum = 0
+powersum = 0
 for game in input.readlines():
     id = int(game.split(':')[0].split(' ')[1])
     impossible = False
-
-    for round in game.split(': ')[1].split('; '):
-        for cubes in [each.split(' ') for each in round.split(', ')]:
-            num = int(cubes[0])
-            color = cubes[1].strip()
-            if num > ref[color]:
-                impossible = True
-                break
-        if impossible:
-            break
-
-    if not impossible:
-        sum += id
-
-print('part 1: ' + str(sum))
-
-input.seek(0)
-
-powersum = 0
-for game in input.readlines():
     maxes = {
         'red': 0,
         'green': 0,
@@ -42,11 +23,16 @@ for game in input.readlines():
         for cubes in [each.split(' ') for each in round.split(', ')]:
             num = int(cubes[0])
             color = cubes[1].strip()
+            if num > ref[color]:
+                impossible = True
             if num > maxes[color]:
                 maxes[color] = num
 
+    if not impossible:
+        sum += id
     powersum += maxes['red'] * maxes['green'] * maxes['blue']
 
+print('part 1: ' + str(sum))
 print('part 2: ' + str(powersum))
 
 input.close()
