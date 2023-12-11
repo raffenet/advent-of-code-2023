0e0dfb325da6bc3c8cfb40ee95a2896e9e57d41c
Ken Raffenetti <raffenet@mcs.anl.gov>
2023-12-10 19:48:16 -0600

    day5: add part 2 solution

---
+++ day5/day5-2.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+input = open('input.txt', 'r')
+#input = open('sample.txt', 'r')
+
+seeds = [int(each) for each in input.readline().split(':')[1].split()]
+seed_ranges = []
+for i in range(0, len(seeds), 2):
+    seed_ranges.append(range(seeds[i], seeds[i] + seeds[i+1]))
+
+maps = []
+for line in input.readlines():
+    if 'map:' in line:
+        maps.append([])
+        continue
+    if line == '\n':
+        continue
+
+    dst = int(line.split()[0])
+    src = int(line.split()[1])
+    length = int(line.split()[2])
+    delta = src - dst
+    maps[-1].append((dst, dst + length, delta))
+maps.reverse()
+
+loc2 = -1
+potential = 0
+while loc2 == -1:
+    orig = potential
+    for m in maps:
+        for r in m:
+            if potential >= r[0] and potential < r[1]:
+                potential += r[2];
+                break;
+    for seed_range in seed_ranges:
+        if potential in seed_range:
+            loc2 = orig
+            break
+    potential = orig + 1
+
+print("part 2:", loc2)
+input.close()
+
+exit(0)
