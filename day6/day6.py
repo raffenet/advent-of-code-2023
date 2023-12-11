0f27e29ca051cfa2a864447068b7c203b7003fc8
Ken Raffenetti <raffenet@mcs.anl.gov>
2023-12-10 20:47:37 -0600

    day6: cleanup part 2 some

--- day6/day6.py
+++ day6/day6.py
@@ -2,7 +2,7 @@
 
 sample = [(7,9),(15,40),(30,200)]
 input = [(60,601),(80,1163),(86,1559),(76,1300)]
-part2 = [(60808676,601116315591300)]
+part2 = (60808676,601116315591300)
 
 error = 1
 for each in input:
@@ -15,15 +15,12 @@ for each in input:
 
 print("part 1:", error)
 
-error = 1
-for each in part2:
-    wins = 0
-    for i in range(1,each[0]):
-        dist = i * (each[0] - i)
-        if (dist > each[1]):
-            wins += 1
-    error *= wins
+wins = 0
+for i in range(1,part2[0]):
+    dist = i * (part2[0] - i)
+    if (dist > part2[1]):
+        wins += 1
 
-print("part 2:", error)
+print("part 2:", wins)
 
 exit(0)
